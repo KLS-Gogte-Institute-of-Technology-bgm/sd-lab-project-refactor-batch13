25b865fca2bbd48497b2b255d0693b010d39cbea
Larii2024 <larina2467m@gmail.com>
2025-03-03 16:10:54 -0500

    Updated the backend and frontend for deployment

--- backend_server/app.js
+++ backend_server/app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-
+require('dotenv').config();
 var logger = require('morgan');
+const cors = require('cors');
+console.log(process.env);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,39 +12,33 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
-const favoriteRouter=require('./routes/favoriteRouter');
-const commentRouter=require('./routes/commentRouter');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-const mongoose = require('mongoose');
+const favoriteRouter = require('./routes/favoriteRouter');
+const commentRouter = require('./routes/commentRouter');
+
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
-const Dishes = require('./models/dishes');
-const Promotions = require('./models/promotions');
-const Leaders = require('./models/leaders');
-const Comments=require('./models/comments');
-const Favorites = require('./models/favorite');
-
-const url = config.mongoUrl;
-const connect = mongoose.connect(url);
+const mongoose = require('mongoose');
+const connect = mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 connect.then((db) => {
     console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
+
 var app = express();
 
-// Secure traffic only
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
-});
+app.use(cors());
 
+if (process.env.NODE_ENV !== 'development') {
+    app.all('*', (req, res, next) => {
+        if (req.secure) {
+            return next();
+        } else {
+            res.redirect(307, 'https://' + req.hostname + req.url);
+        }
+    });
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -51,34 +46,50 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser('12345-67890-09876-54321'));
+
 app.use(passport.initialize());
 
+// API routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
-app.use('/imageUpload',uploadRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 app.use('/favorites', favoriteRouter);
 app.use('/comments', commentRouter);
 
+// Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Serve images from the frontend build/assets/images directory
+app.use('/images', express.static(path.join(__dirname, '../frontend/build', 'assets', 'images')));
+
+
+// // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+// });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
--- backend_server/config.js
+++ backend_server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'secretKey': process.env.SECRET_KEY,
+    'mongoUrl':  process.env.MONGO_URL || 'mongodb://localhost:27017/conFusion'
 }
